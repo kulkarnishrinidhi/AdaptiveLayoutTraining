d6de8f3a3b12c1c718b94da909a88f124a5509a8
RShankar <ravishankar@outlook.in>
2015-07-27 19:19:20 +0530

    Initial Version
    Initial Version

---
+++ AdaptiveLayoutTraining/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdaptiveLayoutTraining
+//
+//  Created by Ravi Shankar on 26/07/15.
+//  Copyright (c) 2015 Ravi Shankar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
